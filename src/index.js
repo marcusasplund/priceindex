f54b32462950db5ae0898866b85c72008335ed4d
Marcus Asplund <marcus@greatname.se>
2018-11-23 17:12:43 +0100

    :fire: :whale: first commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,72 @@
 /* eslint-disable no-unused-vars */
+import Papa from 'papaparse'
+import download from 'downloadjs'
 import { h, app } from 'hyperapp'
 import { priceIndex, priceIndexMonths } from './constants/price-index'
 import './styles/app.scss'
 import 'milligram'
 
+const formatPrice = (price) => {
+  price = Math.round(price)
+  return price.toLocaleString('sv-SE')
+}
+
 const state = {
-  hello: 'hello',
-  result: 0
+  rows: [],
+  year: 2018
+}
+
+const priceToday = (finalyear, year, price) => {
+  return (priceIndex[finalyear] / priceIndex[year]) * price
 }
 
 const actions = {
-  set: x => x
+  set: x => x,
+
+  addRows: (results) => (state, actions) => {
+    let data = results.data[0]
+    let year = data[0]
+    let price = +data[1]
+    if (year && price) {
+      actions.set({
+        rows: state.rows.concat({
+          year: year,
+          price: price
+        })
+      })
+    }
+  },
+
+  downloadCSVFile: (e) => (state, actions) => {
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false)
+    download(Papa.unparse(state.rows, {
+      delimiter: ';'
+    }), 'converted.csv', 'text/csv')
+  },
+
+  parseCSV: (csv) => (state, actions) => {
+    actions.set({
+      rows: []
+    })
+    Papa.parse(csv.data, {
+      download: csv.isFile,
+      header: false,
+      step: (results) => {
+        actions.addRows(results)
+      },
+      complete: () => {
+        console.log('finsihed')
+      }
+    })
+  },
+
+  parseFile: (e) => (state, actions) => {
+    actions.parseCSV({ data: e.target.files[0], isFile: true })
+  },
+
+  parseString: (e) => (state, actions) => {
+    actions.parseCSV({ data: e.target.value })
+  }
 }
 
 const view = (state, actions) => (
@@ -25,48 +81,105 @@ const view = (state, actions) => (
       }, [
         h('h1', {
           class: 'title'
-        }, 'Prisomräknare'),
-        h('ul', {
-          class: 'navigation-list float-right'
-        }, [
-          h('li', {
-            class: 'navigation-item'
-          }, [
-            h('a', {
-              class: 'navigation-link'
-            }, 'test')
-          ]),
-          h('li', {
-            class: 'navigation-item'
-          }, [
-            h('a', {
-              class: 'navigation-link'
-            }, 'test')
-          ]),
-          h('li', {
-            class: 'navigation-item'
-          }, [
-            h('a', {
-              class: 'navigation-link'
-            }, 'logout')
-          ])
-        ]),
-        h('button', {
-          class: 'navbar-burger',
-          'aria-label': 'menu',
-          onclick: e => actions.set({ isClosed: !state.isClosed })
-        }, [
-          h('i', {
-            class: 'material-icons'
-          }, 'menu')
-        ])
+        }, 'Prisomräknare')
       ])
     ]),
     h('div', {
-      class: 'content'
+      class: 'content container'
     }, [
-      h('p', {}, state.hello),
-      Object.keys(priceIndex).map((key, index) => h('p', {}, key + ' ' + priceIndex[key]))
+      h('p', {}, 'Här kan du konvertera längre serier med historiska priser, du kan välja mellan att ladda upp en .csv med tabelldata i ett format där de två första kolumnerna är [år], [pris]. Eller att klistra in den i textrutan nedan. Efter konvertering kan du ladda ned en .csv med resultatet.'),
+      h('p', {}, [
+        h('span', {}, 'Datan kommer från '),
+        h('a', {
+          href: 'https://www.scb.se/hitta-statistik/sverige-i-siffror/prisomraknaren/'
+        }, 'SCB:s prisomräknare'),
+        h('br', {}),
+        h('span', {}, 'Du kan kontakta mig på twitter om du har några frågor '),
+        h('a', {
+          href: 'https://twitter.com/marcusasplund'
+        }, '@marcusasplund')
+      ]),
+      h('div', {
+        class: 'row'
+      }, [
+        h('div', {
+          class: 'column'
+        }),
+        h('div', {
+          class: 'column'
+        }),
+        h('div', {
+          class: 'column'
+        }, [
+          h('label', {}, 'Välj slutår')
+        ])
+      ]),
+      h('div', {
+        class: 'row'
+      }, [
+        h('div', {
+          class: 'column'
+        }, [
+          h('label', {
+            for: 'files',
+            class: 'button file-label'
+          }, 'Ladda upp .csv'),
+          h('input', {
+            type: 'file',
+            id: 'files',
+            accept: '.csv',
+            onchange: e => actions.parseFile(e)
+          })
+        ]),
+        h('div', {
+          class: 'column'
+        }, [
+          h('button', {
+            type: 'button',
+            class: 'download',
+            disabled: state.rows.length === 0,
+            onclick: e => actions.downloadCSVFile(e)
+          }, 'Ladda ned .csv')
+        ]),
+        h('div', {
+          class: 'column'
+        }, [
+          h('select', {
+            value: state.year,
+            onchange: e => actions.set({ year: e.target.value })
+          }, [
+            Object.keys(priceIndex).map((key, index) => h('option', {}, key))
+          ])
+        ])
+      ]),
+      h('label', {}, 'Klistra in data'),
+      h('textarea', {
+        class: 'high',
+        rows: 7,
+        oninput: e => actions.parseString(e),
+        placeholder: `1979, 500
+1980, 600
+1981, 700
+1982, 800`
+      }),
+      h('table', {}, [
+        h('thead', {}, [
+          h('tr', {}, [
+            h('th', {}, 'År'),
+            h('th', {}, 'Historiskt pris'),
+            h('th', {}, `Motsvarande pris ${state.year}`)
+          ])
+        ]),
+        h('tbody', {}, [
+          state.rows.map(r =>
+            h('tr', {}, [
+              h('td', {}, r.year),
+              h('td', {}, `${formatPrice(r.price)} kr`),
+              h('td', {}, `${formatPrice(priceToday(state.year, r.year, r.price))} kr`)
+            ])
+          )
+        ])
+      ])
     ])
   ])
 )
